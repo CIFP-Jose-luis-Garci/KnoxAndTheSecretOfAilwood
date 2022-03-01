494ba078b0ddd7d2a8ebcac21f9fe6fe07f1bd25
Daniel Lopez <daniellopezblanco26@gmail.com>
2022-03-01 17:49:34 +0100

    UI Menu Terminado

--- Unity/Knox_v.0.1/Assets/Scripts/ScriptsUI/BotonesInicio.cs
+++ Unity/Knox_v.0.1/Assets/Scripts/ScriptsUI/BotonesInicio.cs
@@ -2,30 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class BotonesInicio : MonoBehaviour
 {
     [SerializeField] GameObject menuCamera;
     [SerializeField] GameObject trailCamera;
+    public AudioMixer audioMixer;
 
 
     private void Start()
     {
         trailCamera.SetActive(false);
+        gameObject.SetActive(true);
     }
-    public void NuevaPartida()
+    public void MenuInicio()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+    public void Partida()
     { 
         trailCamera.SetActive(true);
+        gameObject.SetActive(false);
         Invoke("InicioJuego", 2f);
 
     }
 
-    public void Cargar()
-    {
-        
-        Invoke("InicioJuego", 2f);
-
-    }
+    
 
     public void Extras()
     {
@@ -45,16 +49,23 @@ public class BotonesInicio : MonoBehaviour
 
     }
 
-    public void MenuInicio()
-    {
-        SceneManager.LoadScene(0);
-
-    }
-
     public void InicioJuego()
     {
         SceneManager.LoadScene(1);
         MusicScript.inst.Pausar();
+    }
+
+    public void SetVolumeMaster(float volumeMaster)
+    {
+        audioMixer.SetFloat("volumeMaster", Mathf.Log10(volumeMaster) * 20);
+    }
+    public void SetVolumeMusic(float volumeMusic)
+    {
+        audioMixer.SetFloat("volumeMusic", Mathf.Log10(volumeMusic) * 20);
+    }
+    public void SetVolumeEffects(float volumeEffects)
+    {
+        audioMixer.SetFloat("volumeEffects", Mathf.Log10(volumeEffects) * 20);
     }
 
 }
